c40b45719fd7d185106e2a7bf2c89db04c271cb2
giovani <giovaniszg@gmail.com>
2022-10-18 22:04:02 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/tareapantalla1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tareapantalla1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        //Color de el fondo de pantalla backgroundColor
+        backgroundColor: Color.fromARGB(255, 241, 239, 239),
+        //Sirve para alinear a lo que pondremos en forma vertical
+        body: Column(
+          //Nos dice que las columnas esten en alineadas en el centro y uno seguido del otro
+          mainAxisAlignment: MainAxisAlignment.center,
+          //utilizamos children para darle propiedades unicas a lo que agregamos ya sea texto o imagen
+          children: [
+            CircleAvatar(
+              radius: 60.0,
+              backgroundColor: Colors.blue,
+              backgroundImage: NetworkImage("https://soft.clbug.com/wp-content/uploads/soft/medical/1615043870.png"),
+            ),
+            //Sisebox para poner altura(height) solo para el codigo que esta encima de ellos
+            //En este caso el SizedBox da altura entre la imagen y el Card
+            SizedBox(
+              height: 90.0,
+              
+              
+            ),
+            //Sirve para dividir por un pequeño espacio
+            Divider(
+              thickness: 0.70,
+              color: Colors.white38,
+              indent: 20.0,
+              endIndent: 85.0,
+            ),
+           
+            
+            //Sirve para poner texto o iconos entre recuadros rectangulos
+            
+            
+            Card (
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
+              child: ListTile(
+                title: Text("Email Address"),
+                subtitle: Text("Username@gmail.com"),
+                leading: Icon(Icons.mail),
+              ),
+            ),
+            //Separa la carta de la otra
+            SizedBox(
+              width: 50.0,
+            ),
+            
+            
+           
+            Card (
+              
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
+              child: ListTile(
+                //Texto que esta encima y es titulo
+                title: Text("Password"),
+                //Texto que esta debajo y es subtitulo
+                subtitle: Text("***"),
+                //Significa que este icono va al comienzo de las letras o lo demas
+                leading: Icon(
+                  Icons.lock,
+                ),
+                trailing:
+                    Icon(Icons.remove_red_eye
+                    ),
+              ),
+            ),
+            SizedBox(
+              width: 50.0,
+            ),
+            Card(
+              color: Color.fromARGB(255, 2, 42, 74),
+              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+              child: Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    //Tamaño Letra
+                    fontSize: 30,
+                    color: Colors.white
+                  ),
+                  ),
+              ),
+              ),
+ 
+              SizedBox(
+              height: 10.0,
+            ),
+             Container(
+margin: EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0),
+child: Text(
+"Signup                            Forgot Password?",
+style: TextStyle(
+color: Color.fromARGB(255, 138, 134, 134),
+),
+),
+), 
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
